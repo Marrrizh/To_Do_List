2ca041d8397ee145b07e67a50faae439c0a532e4
Maria S <mariz87@yandex.ru>
2025-11-17 11:09:55 +0300

    Found category of tasks

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let CategoryHeader=document.querySelectorAll('.category-header.clickable');
 
 function findCategory(event) {
     const clickedHeader = event.currentTarget; 
+  
    
  let arrowchange=clickedHeader.querySelector('.arrow');
 
@@ -10,8 +11,9 @@ if (arrowchange.textContent === "▶") {
     arrowchange.textContent = "▼";
 } else {
     arrowchange.textContent = "▶";
-}
-
+};
+let AllCategories=clickedHeader.closest('.category');
+let CategoryTasks=AllCategories.querySelector('.tasks');
 };
 
 CategoryHeader.forEach(function(header) {
